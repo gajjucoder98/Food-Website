6759157b262c04fd1b0e6787b00e3ce1df207543
gajjucoder98 <161016330+gajjucoder98@users.noreply.github.com>
2024-03-01 21:48:53 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var x = 0;
+var y = 1;
+var hamburger = document.querySelector("#hamburger");
+var menu = document.querySelector(".menu");
+var doted = document.querySelector("#dote");
+var piyu = document.querySelector(".piyu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("open-burger");
+  if (menu.style.transform == "") {
+    menu.style.transform = "translateX(0px)";
+    piyu.style.transform = "";
+    
+  } else {
+    menu.style.transform = "";
+  }
+});
+
+doted.addEventListener("click", function () {
+  if (piyu.style.transform == "") {
+    piyu.style.transform = "translateX(0px)";
+    menu.style.transform = "";
+    piyu.style.opacity = 1;
+    hamburger.classList.remove("open-burger");
+
+  } else {
+    piyu.style.transform = "";
+  }
+});
